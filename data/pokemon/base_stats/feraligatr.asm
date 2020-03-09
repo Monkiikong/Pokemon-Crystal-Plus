5f0336032e18853fd10352edb698e0680e30abad
Monkiikong <47468807+Monkiikong@users.noreply.github.com>
2020-03-09 16:01:42 +1100

    Update feraligatr.asm

--- data/pokemon/base_stats/feraligatr.asm
+++ data/pokemon/base_stats/feraligatr.asm
@@ -4,7 +4,7 @@
 	evs  0,   2,   0,   0,   0,   0
 	;   hp  atk  def  spd  sat  sdf
 
-	db WATER, WATER ; type
+	db WATER, DARK ; type
 	db 45 ; catch rate
 	db 210 ; base exp
 	db NO_ITEM, NO_ITEM ; items
