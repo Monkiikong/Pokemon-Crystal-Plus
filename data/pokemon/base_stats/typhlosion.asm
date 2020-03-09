b841f44314ed2c5a6a9ec87c70f4bcb1cc920c49
Monkiikong <47468807+Monkiikong@users.noreply.github.com>
2020-03-09 16:02:50 +1100

    Update typhlosion.asm

--- data/pokemon/base_stats/typhlosion.asm
+++ data/pokemon/base_stats/typhlosion.asm
@@ -4,7 +4,7 @@
 	evs  0,   0,   0,   0,   3,   0
 	;   hp  atk  def  spd  sat  sdf
 
-	db FIRE, FIRE ; type
+	db FIRE, GROUND ; type
 	db 45 ; catch rate
 	db 209 ; base exp
 	db NO_ITEM, NO_ITEM ; items
